0d38aee111fc83ca56cf67826e9c2b796c2ec429
BrunoNotes <brunonotes7@gmail.com>
2023-12-02 13:44:20 -0300

    day 1 second half

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -1,29 +1,72 @@
-use std::{env, fs, i32};
+use std::{env, fs};
 
 fn main() {
     let file_path = format!(
         "{}/part1/src/input.txt",
         env::current_dir().unwrap().display()
     );
+    // let file = fs::File::open(file_path).unwrap();
+    // let mut buf_reader = BufReader::new(file);
+    // let mut input = String::new();
+    // buf_reader.read_to_string(&mut input).unwrap();
+
     let input = fs::read_to_string(file_path).unwrap();
 
-    let mut result: i32 = 0;
-    for line in input.lines() {
-        // let test = line.chars().skip_while(|c| c.is_digit(10)).map(|c| c);
-        let mut numbers_from_line: Vec<char> = Vec::new();
-        for c in line.chars() {
-            if c.is_digit(10) {
-                // println!("{:?}", c);
-                numbers_from_line.push(c.into());
-            }
-        }
-        // println!("{:?}", numbers_from_line);
-        let first = numbers_from_line.first().unwrap();
-        let last = numbers_from_line.last().unwrap();
+    //     let input = "two1nine
+    // eightwothree
+    // abcone2threexyz
+    // xtwone3four
+    // 4nineeightseven2
+    // zoneight234
+    // 7pqrstsixteen
+    // ".to_string();
 
-        let calibration: i32 = format!("{}{}", first, last).parse::<i32>().unwrap();
-        // println!("{}", calibration);
-        result += calibration;
-    }
-    println!("{:?}", result);
+    // let mut n_line = 1;
+
+    let output: u32 = input
+        .lines()
+        .map(|line| {
+            let line_replace = (0..line.len()).filter_map(|index| {
+                let line_by_index = &line[index..];
+
+                let result = if line_by_index.starts_with("one") {
+                    Some('1')
+                } else if line_by_index.starts_with("two") {
+                    Some('2')
+                } else if line_by_index.starts_with("three") {
+                    Some('3')
+                } else if line_by_index.starts_with("four") {
+                    Some('4')
+                } else if line_by_index.starts_with("five") {
+                    Some('5')
+                } else if line_by_index.starts_with("six") {
+                    Some('6')
+                } else if line_by_index.starts_with("seven") {
+                    Some('7')
+                } else if line_by_index.starts_with("eight") {
+                    Some('8')
+                } else if line_by_index.starts_with("nine") {
+                    Some('9')
+                } else {
+                    line_by_index.chars().next()
+                };
+                result
+            });
+
+            let mut x = line_replace.filter_map(|c| c.to_digit(10));
+
+            let first = x.next().unwrap();
+            // let last = x.last().unwrap();
+
+            match x.last() {
+                Some(n) => format!("{first}{n}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .unwrap()
+        })
+        .sum();
+
+    // let output = input.lines().map(process_line).sum::<u32>();
+    println!("{output}");
 }
