6e4631bace25c451258641ff80c5e9c4c9224147
BrunoNotes <brunonotes7@gmail.com>
2023-12-03 19:38:16 -0300

    day 2 second half

--- part2/src/main.rs
+++ part2/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env, fmt::format, fs};
+use std::{collections::HashMap, env, fs};
 
 const TEST_INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -25,11 +25,6 @@ struct CubeColorQuantity {
     blue: i32,
 }
 
-// #[derive(Debug, Hash, Eq, PartialEq)]
-// struct Game {
-//     game: String,
-// }
-
 fn main() {
     let file_path = format!(
         "{}/part2/src/input.txt",
@@ -39,6 +34,91 @@ fn main() {
     let input = fs::read_to_string(file_path).unwrap();
     // let input = TEST_INPUT.to_string();
 
+    let games = game_parser(input);
+
+    // println!("{:#?}", games);
+
+    // let first_half = first_half(games);
+    // println!("{:?}", first_half);
+
+    let second_half = second_half(games);
+    println!("{:#?}", second_half);
+}
+
+fn second_half(games: HashMap<String, Vec<CubeColorQuantity>>) -> i32 {
+    let min_viable_game: HashMap<_, _> = games
+        .iter()
+        .map(|(game, cubes)| {
+            // println!("{:?}", cubes);
+            let mut red = 0;
+            let mut green = 0;
+            let mut blue = 0;
+
+            for cube in cubes {
+                if cube.red > red {
+                    red = cube.red;
+                }
+                if cube.blue > blue {
+                    blue = cube.blue;
+                }
+                if cube.green > green {
+                    green = cube.green;
+                }
+            }
+
+            let max_cube_quantity = CubeColorQuantity {
+                red: red,
+                blue: blue,
+                green: green,
+            };
+
+            return (game, max_cube_quantity);
+        })
+        .collect();
+
+    let game_power: HashMap<_, _> = min_viable_game
+        .iter()
+        .map(|(game, cubes)| {
+            let cube_power = cubes.red * cubes.green * cubes.blue;
+            return (game.parse::<i32>().unwrap(), cube_power);
+        })
+        .collect();
+
+    let total: i32 = game_power.iter().map(|(game, power)| power).sum();
+    return total;
+}
+fn first_half(game: HashMap<String, Vec<CubeColorQuantity>>) -> i32 {
+    let max_cube_quantity = CubeColorQuantity {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+
+    let mut total_valid = 0;
+
+    for (round, cube) in game {
+        let mut is_valid: bool = true;
+
+        for quantity in cube {
+            if quantity.red > max_cube_quantity.red {
+                is_valid = false
+            } else if quantity.blue > max_cube_quantity.blue {
+                is_valid = false
+            } else if quantity.green > max_cube_quantity.green {
+                is_valid = false
+            }
+        }
+
+        if is_valid == true {
+            // println!("{:?}", round);
+            total_valid += round.parse::<i32>().unwrap();
+        }
+    }
+
+    return total_valid;
+}
+
+fn game_parser(input: String) -> HashMap<String, Vec<CubeColorQuantity>> {
     let game_cubes: Vec<ElfGameDesc> = input
         .lines()
         .map(|line| {
@@ -136,36 +216,5 @@ fn main() {
         })
         .collect();
 
-    let max_cube_quantity = CubeColorQuantity {
-        red: 12,
-        green: 13,
-        blue: 14,
-    };
-
-    let mut total_valid = 0;
-    // println!("{:?}", game);
-    for (round, cube) in game {
-        let mut is_valid: bool= true;
-
-        for quantity in cube {
-            if quantity.red > max_cube_quantity.red {
-                is_valid = false
-            } else if quantity.blue > max_cube_quantity.blue {
-                is_valid = false
-            } else if quantity.green > max_cube_quantity.green {
-                is_valid = false
-            } 
-        }
-
-        // println!(
-        //     "Round: {:?}, Cube: {:?}, Valid: {:?}",
-        //     round,  cube, is_valid
-        // );
-
-        if is_valid == true {
-            println!("{:?}", round);
-            total_valid += round.parse::<i32>().unwrap();
-        }
-    }
-    println!("Total valid: {:?}", total_valid);
+    return game;
 }
